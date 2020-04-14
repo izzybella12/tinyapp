948340ce29c1b9443197a97c73d8a3e1ae3af342
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-04-14 01:17:37 +0000

    Implemented HTML POST

--- express_server.js
+++ express_server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca", 
@@ -34,6 +40,11 @@ app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   let templateVars = {shortURL, longURL: urlDatabase[shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
